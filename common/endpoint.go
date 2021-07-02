55e0c52ae92993bdb909f20ddd0f07b701e57a5b
Fangyi Zhou <me@fangyi.io>
2021-07-02 17:38:30 +0800

    Make an interface for endpoints

--- common/endpoint.go
+++ common/endpoint.go
@@ -2,12 +2,13 @@ package common
 
 import (
 	"context"
+	"log"
+	"sync"
+
 	"github.com/fangyi-zhou/mpst-tracing/labels"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-	"log"
-	"sync"
 )
 
 var (
@@ -23,59 +24,86 @@ type Message struct {
 	Value interface{}
 }
 
-type EndPoint struct {
-	Name     string
-	run      func(self *EndPoint, group *sync.WaitGroup)
-	partners map[string]*EndPoint
+type EndPoint interface {
+	Name() string
+	Tracer() trace.Tracer
+	Send(ctx context.Context, partner string, message Message) error
+	Recv(ctx context.Context, partner string) (Message, error)
+	Connect(other EndPoint) error
+	Run(group *sync.WaitGroup)
+}
+
+type QueueEndPoint struct {
+	name     string
+	run      func(self EndPoint, group *sync.WaitGroup)
+	partners map[string]*QueueEndPoint
 	buffer   map[string]chan Message
-	Tracer   trace.Tracer
+	tracer   trace.Tracer
 }
 
-func MakeEndPoint(name string, runFunc func(self *EndPoint, group *sync.WaitGroup)) *EndPoint {
-	return &EndPoint{
-		Name:     name,
+func MakeQueueEndPoint(name string, runFunc func(self EndPoint, group *sync.WaitGroup)) EndPoint {
+	return &QueueEndPoint{
+		name:     name,
 		run:      runFunc,
-		partners: make(map[string]*EndPoint),
+		partners: make(map[string]*QueueEndPoint),
 		buffer:   make(map[string]chan Message),
-		Tracer:   otel.Tracer(name),
+		tracer:   otel.Tracer(name),
 	}
 }
 
-func connectEndpoints(ep1 *EndPoint, ep2 *EndPoint) {
-	if ep1.Name == ep2.Name {
-		log.Panicf("Cannot connect two endpoints with same name %s", ep1.Name)
-	}
-	if _, exists := ep1.partners[ep2.Name]; exists {
-		log.Panicf("Cannot connect two connected endpoints, %s and %s", ep1.Name, ep2.Name)
-	}
-	if _, exists := ep2.partners[ep1.Name]; exists {
-		log.Panicf("Cannot connect two connected endpoints, %s and %s", ep1.Name, ep2.Name)
-	}
-	ep1.partners[ep2.Name] = ep2
-	ep1.buffer[ep2.Name] = make(chan Message, 1)
-	ep2.partners[ep1.Name] = ep1
-	ep2.buffer[ep1.Name] = make(chan Message, 1)
+func (e *QueueEndPoint) Run(group *sync.WaitGroup) {
+	e.run(e, group)
 }
 
-func (e *EndPoint) Send(ctx context.Context, partner string, message Message) {
+func (e *QueueEndPoint) Name() string {
+	return e.name
+}
+
+func (e *QueueEndPoint) Tracer() trace.Tracer {
+	return e.tracer
+}
+
+func connectQueueEndpoints(ep1 *QueueEndPoint, ep2 *QueueEndPoint) {
+	if ep1.name == ep2.name {
+		log.Panicf("Cannot connect two endpoints with same name %s", ep1.name)
+	}
+	if _, exists := ep1.partners[ep2.name]; exists {
+		log.Panicf("Cannot connect two connected endpoints, %s and %s", ep1.name, ep2.name)
+	}
+	if _, exists := ep2.partners[ep1.name]; exists {
+		log.Panicf("Cannot connect two connected endpoints, %s and %s", ep1.name, ep2.name)
+	}
+	ep1.partners[ep2.name] = ep2
+	ep1.buffer[ep2.name] = make(chan Message, 1)
+	ep2.partners[ep1.name] = ep1
+	ep2.buffer[ep1.name] = make(chan Message, 1)
+}
+
+func (e *QueueEndPoint) Connect(other EndPoint) error {
+	connectQueueEndpoints(e, other.(*QueueEndPoint))
+	return nil
+}
+
+func (e *QueueEndPoint) Send(ctx context.Context, partner string, message Message) error {
 	var span trace.Span
-	_, span = e.Tracer.Start(ctx, "Send")
+	_, span = e.tracer.Start(ctx, "Send")
 	defer span.End()
 	span.SetAttributes(msgLabelKey.String(message.Label), partnerKey.String(partner), actionKey.String(actionSend))
 	if _, exists := e.partners[partner]; !exists {
-		log.Panicf("%s is trying to send a message to an unconnected endpoint %s", e.Name, partner)
+		log.Panicf("%s is trying to send a message to an unconnected endpoint %s", e.name, partner)
 	}
-	e.partners[partner].buffer[e.Name] <- message
+	e.partners[partner].buffer[e.name] <- message
+	return nil
 }
 
-func (e *EndPoint) Recv(ctx context.Context, partner string) Message {
+func (e *QueueEndPoint) Recv(ctx context.Context, partner string) (Message, error) {
 	var span trace.Span
-	_, span = e.Tracer.Start(ctx, "Recv")
+	_, span = e.tracer.Start(ctx, "Recv")
 	defer span.End()
 	if _, exists := e.partners[partner]; !exists {
-		log.Panicf("%s is trying to send a message to an unconnected endpoint %s", e.Name, partner)
+		log.Panicf("%s is trying to send a message to an unconnected endpoint %s", e.name, partner)
 	}
 	message := <-e.buffer[partner]
 	span.SetAttributes(msgLabelKey.String(message.Label), partnerKey.String(partner), actionKey.String(actionRecv))
-	return message
+	return message, nil
 }
--- twobuyer/two_buyer.go
+++ twobuyer/two_buyer.go
@@ -3,25 +3,28 @@ package twobuyer
 import (
 	"context"
 	"fmt"
-	"github.com/fangyi-zhou/mpst-examples/common"
-	"go.opentelemetry.io/otel/trace"
 	"math/rand"
 	"sync"
+
+	"github.com/fangyi-zhou/mpst-examples/common"
+	"go.opentelemetry.io/otel/trace"
 )
 
-func runA(self *common.EndPoint, wg *sync.WaitGroup) {
+func runA(self common.EndPoint, wg *sync.WaitGroup) {
 	defer wg.Done()
 	ctx := context.Background()
 	var span trace.Span
-	ctx, span = self.Tracer.Start(ctx, "TwoBuyer Endpoint A")
+	ctx, span = self.Tracer().Start(ctx, "TwoBuyer Endpoint A")
 	defer span.End()
 	// Send query to S
 	var query = rand.Intn(100)
 	fmt.Println("A: Sending query", query)
 	self.Send(ctx, "S", common.Message{Label: "query", Value: query})
 	// Receive a quote
-	var quote = self.Recv(ctx, "S").Value.(int)
-	var otherShare = self.Recv(ctx, "B").Value.(int)
+	var quoteMsg, _ = self.Recv(ctx, "S")
+	var quote = quoteMsg.Value.(int)
+	var otherShareMsg, _ = self.Recv(ctx, "B")
+	var otherShare = otherShareMsg.Value.(int)
 	if otherShare*2 >= quote {
 		// 1 stands for ok
 		self.Send(ctx, "S", common.Message{Label: "buy", Value: 1})
@@ -30,19 +33,21 @@ func runA(self *common.EndPoint, wg *sync.WaitGroup) {
 	}
 }
 
-func runABad(self *common.EndPoint, wg *sync.WaitGroup) {
+func runABad(self common.EndPoint, wg *sync.WaitGroup) {
 	defer wg.Done()
 	ctx := context.Background()
 	var span trace.Span
-	ctx, span = self.Tracer.Start(ctx, "TwoBuyer Endpoint A")
+	ctx, span = self.Tracer().Start(ctx, "TwoBuyer Endpoint A")
 	defer span.End()
 	// Send query to S
 	var query = rand.Intn(100)
 	fmt.Println("A: Sending query", query)
 	self.Send(ctx, "S", common.Message{Label: "query", Value: query})
 	// Receive a quote
-	var quote = self.Recv(ctx, "S").Value.(int)
-	var otherShare = self.Recv(ctx, "B").Value.(int)
+	var quoteMsg, _ = self.Recv(ctx, "S")
+	var quote = quoteMsg.Value.(int)
+	var otherShareMsg, _ = self.Recv(ctx, "B")
+	var otherShare = otherShareMsg.Value.(int)
 	if otherShare*2 >= quote {
 		// 1 stands for ok
 		self.Send(ctx, "S", common.Message{Label: "purchase", Value: 1})
@@ -51,20 +56,22 @@ func runABad(self *common.EndPoint, wg *sync.WaitGroup) {
 	}
 }
 
-func runS(self *common.EndPoint, wg *sync.WaitGroup) {
+func runS(self common.EndPoint, wg *sync.WaitGroup) {
 	defer wg.Done()
 	ctx := context.Background()
 	var span trace.Span
-	ctx, span = self.Tracer.Start(ctx, "TwoBuyer Endpoint S")
+	ctx, span = self.Tracer().Start(ctx, "TwoBuyer Endpoint S")
 	defer span.End()
 	// Receive a query
-	var query = self.Recv(ctx, "A").Value.(int)
+	var queryMsg, _ = self.Recv(ctx, "A")
+	var query = queryMsg.Value.(int)
 	// Send a quote
 	var quote = query * 2
 	fmt.Println("S: Sending quote", quote)
 	self.Send(ctx, "A", common.Message{Label: "quote", Value: quote})
 	self.Send(ctx, "B", common.Message{Label: "quote", Value: quote})
-	var decision = self.Recv(ctx, "A").Value.(int)
+	var decisionMsg, _ = self.Recv(ctx, "A")
+	var decision = decisionMsg.Value.(int)
 	if decision == 1 {
 		fmt.Println("Succeed!")
 	} else {
@@ -72,25 +79,26 @@ func runS(self *common.EndPoint, wg *sync.WaitGroup) {
 	}
 }
 
-func runB(self *common.EndPoint, wg *sync.WaitGroup) {
+func runB(self common.EndPoint, wg *sync.WaitGroup) {
 	defer wg.Done()
 	ctx := context.Background()
 	var span trace.Span
-	ctx, span = self.Tracer.Start(ctx, "TwoBuyer Endpoint B")
+	ctx, span = self.Tracer().Start(ctx, "TwoBuyer Endpoint B")
 	defer span.End()
 	// Receive a quote
-	var quote = self.Recv(ctx, "S").Value.(int)
+	var quoteMsg, _ = self.Recv(ctx, "S")
+	var quote = quoteMsg.Value.(int)
 	// Propose a share
 	var share = quote/2 + rand.Intn(10) - 5
 	fmt.Println("B: Proposing share", share)
 	self.Send(ctx, "A", common.Message{Label: "share", Value: share})
 }
 
-func MakeEndpoints() []*common.EndPoint {
-	a := common.MakeEndPoint("A", runA)
-	b := common.MakeEndPoint("B", runB)
-	s := common.MakeEndPoint("S", runS)
-	return []*common.EndPoint{a, b, s}
+func MakeEndpoints() []common.EndPoint {
+	a := common.MakeQueueEndPoint("A", runA)
+	b := common.MakeQueueEndPoint("B", runB)
+	s := common.MakeQueueEndPoint("S", runS)
+	return []common.EndPoint{a, b, s}
 }
 
 func RunAll() {
@@ -101,9 +109,9 @@ func RunAllBad() {
 	common.RunEndpoints(common.InitOtlpTracer, MakeBadEndpoints())
 }
 
-func MakeBadEndpoints() []*common.EndPoint {
-	a := common.MakeEndPoint("A", runABad)
-	b := common.MakeEndPoint("B", runB)
-	s := common.MakeEndPoint("S", runS)
-	return []*common.EndPoint{a, b, s}
+func MakeBadEndpoints() []common.EndPoint {
+	a := common.MakeQueueEndPoint("A", runABad)
+	b := common.MakeQueueEndPoint("B", runB)
+	s := common.MakeQueueEndPoint("S", runS)
+	return []common.EndPoint{a, b, s}
 }
