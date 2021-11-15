eaa6ac398c5f4c0762da495d5e0cb01487f965fb
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2021-11-15 13:59:54 +0000

    Upgrade dependencies

--- common/tracers.go
+++ common/tracers.go
@@ -3,28 +3,23 @@ package common
 import (
 	"context"
 	"log"
-	"time"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpgrpc"
-	"go.opentelemetry.io/otel/exporters/stdout"
-	"go.opentelemetry.io/otel/exporters/trace/jaeger"
-	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 	"google.golang.org/grpc"
 )
 
 // https://github.com/open-telemetry/opentelemetry-go/blob/master/example/namedtracer/main.go
 func InitStdoutTracer() func() {
 	var err error
-	exp, err := stdout.NewExporter(stdout.WithPrettyPrint())
+	exp, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
 	if err != nil {
 		log.Panicf("failed to initialize stdout exporter %v\n", err)
 	}
@@ -42,7 +37,7 @@ const endpointUri = "http://localhost:14268/api/traces"
 // https://github.com/open-telemetry/opentelemetry-go/blob/master/example/jaeger/main.go
 func InitJaegerTracer() func() {
 	// Create and install Jaeger export pipeline.
-	exp, err := jaeger.NewRawExporter(
+	exp, err := jaeger.New(
 		jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpointUri)),
 	)
 	if err != nil {
@@ -53,6 +48,7 @@ func InitJaegerTracer() func() {
 		trace.WithBatcher(exp),
 		// Record information about this application in an Resource.
 		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
 			semconv.ServiceNameKey.String("TwoBuyer"),
 		)),
 	)
@@ -69,12 +65,12 @@ func InitOtlpTracer() func() {
 	// `localhost:30080` endpoint. Otherwise, replace `localhost` with the
 	// endpoint of your cluster. If you run the app inside k8s, then you can
 	// probably connect directly to the service through dns
-	driver := otlpgrpc.NewDriver(
-		otlpgrpc.WithInsecure(),
-		otlpgrpc.WithEndpoint("localhost:30080"),
-		otlpgrpc.WithDialOption(grpc.WithBlock()), // useful for testing
+	driver := otlptracegrpc.NewClient(
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint("localhost:30080"),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()), // useful for testing
 	)
-	exp, err := otlp.NewExporter(ctx, driver)
+	exp, err := otlptrace.New(ctx, driver)
 	if err != nil {
 		log.Fatalf("failed to create exporter %e", err)
 	}
@@ -96,32 +92,12 @@ func InitOtlpTracer() func() {
 		trace.WithSpanProcessor(bsp),
 	)
 
-	cont := controller.New(
-		processor.New(
-			simple.NewWithExactDistribution(),
-			exp,
-		),
-		controller.WithExporter(exp),
-		controller.WithCollectPeriod(2*time.Second),
-	)
-
 	// set global propagator to tracecontext (the default is no-op).
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 	otel.SetTracerProvider(tracerProvider)
-	global.SetMeterProvider(cont.MeterProvider())
-	if err := cont.Start(context.Background()); err != nil {
-		log.Panicf("failed to start controller %e", err)
-	}
-
 	return func() {
-		err = cont.Stop(context.Background())
-		if err != nil {
-			log.Panicf("Failed to stop controller, %v\n", err)
-		}
-
-		err = tracerProvider.Shutdown(ctx)
-		if err != nil {
-			log.Panicf("Failed to shutdown TraceProvider, %v\n", err)
+		if err := tracerProvider.Shutdown(ctx); err != nil {
+			log.Fatalf("failed to shutdown TracerProvider, %e", err)
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,32 +3,28 @@ module github.com/fangyi-zhou/mpst-examples
 go 1.17
 
 require (
-	github.com/fangyi-zhou/mpst-tracing v0.0.0-20210623022711-e5a037806520
-	go.opentelemetry.io/otel v0.20.0
-	go.opentelemetry.io/otel/exporters/otlp v0.20.0
-	go.opentelemetry.io/otel/exporters/stdout v0.20.0
-	go.opentelemetry.io/otel/exporters/trace/jaeger v0.20.0
-	go.opentelemetry.io/otel/metric v0.20.0
-	go.opentelemetry.io/otel/sdk v0.20.0
-	go.opentelemetry.io/otel/sdk/metric v0.20.0
-	go.opentelemetry.io/otel/trace v0.20.0
-	google.golang.org/grpc v1.38.0
+	github.com/fangyi-zhou/mpst-tracing v0.0.0-20211026231637-2aef111310a3
+	go.opentelemetry.io/otel v1.2.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.2.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.2.0
+	go.opentelemetry.io/otel/sdk v1.2.0
+	go.opentelemetry.io/otel/trace v1.2.0
+	google.golang.org/grpc v1.42.0
 )
 
+require github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	go.opentelemetry.io/otel/sdk/export/metric v0.20.0 // indirect
-	go.opentelemetry.io/proto/otlp v0.7.0 // indirect
-	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
-	golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.2.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.2.0
+	go.opentelemetry.io/proto/otlp v0.11.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20211113001501-0c823b97ae02 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211112145013-271947fe86fd // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
 
 replace github.com/fangyi-zhou/mpst-tracing v0.0.0 => ../mpst-tracing
